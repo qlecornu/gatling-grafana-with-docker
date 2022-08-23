b9fec3b72520ddaeac2a5daccf860e1d617f49fa
qlecornu <qlecornu@gatling.io>
2022-08-23 12:39:30 +0200

    update grafana plugin

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ until [ "`docker inspect -f {{.State.Running}} grafana`"=="true" ]; do
     echo "wait for a docker container to be up and running";
 done;
 echo "INSTALL GATLING PLUGIN"
-docker exec -it --user root grafana grafana-cli --pluginUrl https://downloads.gatling.io/releases/frontline-grafana-bundle/1.16.4/frontline-grafana-bundle-1.16.4-bundle.zip plugins install frontline
+docker exec -it --user root grafana grafana-cli --pluginUrl https://downloads.gatling.io/releases/frontline-grafana-bundle/1.16.5/frontline-grafana-bundle-1.16.5-bundle.zip plugins install frontline
 docker exec -it --user root grafana sed -i 's/allow_loading_unsigned_plugins =/allow_loading_unsigned_plugins = frontline/g' /usr/share/grafana/conf/defaults.ini
 echo "RESTART GRAFANA"
 docker compose restart
